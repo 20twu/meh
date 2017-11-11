76f292626a10fd29d8fd33adba9bc8ee236a421c
TinTin <32924832+20twu@users.noreply.github.com>
2017-11-11 11:30:05 +0100

    Don't know, the heck?!?

---
+++ tryThis.java
@@ -0,0 +1,37 @@
+
+/**
+ * Write a description of class tryThis here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class tryThis
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class tryThis
+     */
+    public tryThis()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+        public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+    public static void main(String[] arg){
+        System.out.println("Hello World");
+        System.out.println();
+    }
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+
+}
