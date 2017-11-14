18deff220440ab189603b2c40bd802b43ad05a1f
ACosmicPyromanialPsychopath <ACosmicPyromanialPsychopath@gmail.com>
2017-11-14 11:10:22 +0100

    sdafasfajspdofjadspo
    afjdasofjsaoifjpsodfsadfsadfasdasasdsadasdfsafasdfas

--- life.java
+++ life.java
@@ -5,83 +5,80 @@ public class life extends prelife{
     boolean addiction = false;
     boolean living = false;
     
-    int wealth = 0;
-    int intel = 0;
+    int money = 0;
+    int intelligence = 0;
     
     public void getBirthed()
-<<<<<<< HEAD
     {   this.parents();
         if(parent == 5)
         {
             money += 200000;
         }
         if(parent == 4)
-=======
-    {   living = true;
+        {living = true;
         if(this.parentIsRich())
-        {wealth+=200000;
-         intel+=10;
+        {money+=200000;
+         intelligence+=10;
         }
         if(this.parentsAreFailures())
-        {wealth-= 200000;
+        {money-= 200000;
          addiction = true;
-         intel-=10;
+         intelligence-=10;
         }
-        while(wealth>0)
+        while(money>0)
         {this.privateSchool();
-         intel+=2;
-         wealth-=1000;
+         intelligence+=2;
+         money-=1000;
         }
-        /*if(this.parentsAreRich)
+        if(this.parentsAreRich)
         {
-            wealthey += 200000;
-        //}
-        /*if(this.parentsAreNormal)
->>>>>>> 9871b836095e71ea673c7dc0c8e87429d4ced54e
+            money += 200000;
+        }
+        if(this.parentsAreNormal)
         {
-            wealthey += 100000;
+            money += 100000;
         }
         if(parent == 2)
         {
-            wealthey -= 100000;
+            money -= 100000;
         }
         if(parent == 1)
         {
-            wealthey -= 200000;
+            money -= 200000;
             addiction = true;
-<<<<<<< HEAD
         }
         life = true;
-=======
-        }*/
->>>>>>> 9871b836095e71ea673c7dc0c8e87429d4ced54e
         }
+    }
+        
     
     public void goToSchool()
     {
         for(int year = 0; year<12; year++)
         {
-            if(wealth>0)
+            if(money>0)
             {this.privateSchool();
             }
             else{
-                if(wealth>-100001)
+                if(money>-100001)
                 {this.publicSchool();
                 }
                 else{
                     this.work();
                 }
+            }
         }
     }
-   }
-   public void findWork()
-   {
+   
+
+    public void findWork()
+    {
        
     }
    
    
     public static void main(String[] arg){
 
-   }
+    }
 
 }
